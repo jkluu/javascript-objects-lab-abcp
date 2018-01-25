97ee41f4d343a05de113d071b01bd3a58e6bc609
Jessica <jessica.k.luu@gmail.com>
2018-01-25 11:26:31 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(obj, key, value) {
+  return Object.assign({}, obj, { [key]: value });
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  obj[key] = value
+  return obj;
+}
+
+function deleteFromObjectByKey(obj, key) {
+  var newObject = delete obj.key;
+  return newObject;
+}
